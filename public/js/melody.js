8f7e1d572fcd17b564a07a83469d404a9d9734bc
Timothy Boronczyk <tboronczyk@gmail.com>
2013-06-12 14:55:51 -0400

    JS lint clean up

--- public/js/melody.js
+++ public/js/melody.js
@@ -1,29 +1,28 @@
 jQuery(document).ready(function ($) {
+    var staff = document.getElementById("staff"),
+        paramForm = $("#paramForm");
 
     function msg(title, text) {
         $("#msg").show().html(
-            "<strong>" + title + "</strong><br>" + text);
+            "<strong>" + title + "</strong><br>" + text
+        );
     }
-
-    var staff = document.getElementById("staff");
 
     staff.clear = function (w, h) {
         staff.width = w;
         staff.height = h;
-        this.getContext('2d').clearRect(0, 0, w, h);
+        this.getContext("2d").clearRect(0, 0, w, h);
     };
 
     staff.drawNote = function (note, x, y) {
         var img = new Image(),
-            ctx = this.getContext('2d');
+            ctx = this.getContext("2d");
 
-        img.src = '/img/' + encodeURIComponent(note) + '.png';
+        img.src = "/img/" + encodeURIComponent(note) + ".png";
         img.onload = function () {
             ctx.drawImage(img, x, y);
-        }
-    }
-
-    var paramForm = $("#paramForm");
+        };
+    };
 
     paramForm.submit(function () {
         var result = $("#result"),
@@ -31,37 +30,35 @@ jQuery(document).ready(function ($) {
             vote = $("#vote");
 
         $.ajax({
-                type: paramForm.attr("method"),
-                url: paramForm.attr("action"),
-                data: paramForm.serialize(),
-                datType: 'json'
-            })
-            .fail(function (jqXHR, resp, err) {
-                msg(jqXHR.status + " " + err, jqXHR.responseText);
-                result.hide();
-                staff.clear(0, 0);
-            })
-            .done(function (resp) {
-                var i = 0,
-                    len = resp.melody.length,
-                    offset = 34,
-                    pos = 55,
-                    height = 90,
-                    encoded = encodeURIComponent(resp.melody.join('.'));
+            type: paramForm.attr("method"),
+            url: paramForm.attr("action"),
+            data: paramForm.serialize(),
+            datType: "json"
+        }).fail(function (jqXHR, resp, err) {
+            msg(jqXHR.status + " " + err, jqXHR.responseText);
+            result.hide();
+            staff.clear(0, 0);
+        }).done(function (resp) {
+            var i = 0,
+                len = resp.melody.length,
+                offset = 34,
+                pos = 55,
+                height = 90,
+                encoded = encodeURIComponent(resp.melody.join("."));
 
-                staff.clear(pos + offset * len, height);
-                staff.drawNote('clef', 0, 0);
-                for (; i < len; i++) {
-                    staff.drawNote(resp.melody[i], pos, 0);
-                    pos += offset;
-                }
+            staff.clear(pos + offset * len, height);
+            staff.drawNote("clef", 0, 0);
+            for (; i < len; i++) {
+                staff.drawNote(resp.melody[i], pos, 0);
+                pos += offset;
+            }
 
-                midi.attr('href', '/melody/midi/' + encoded);
-                vote.attr('data-melody', encoded).show();
-                result.show();
+            midi.attr("href", "/melody/midi/" + encoded);
+            vote.attr("data-melody", encoded).show();
+            result.show();
 
-                msg("Data", resp.melody.join(' '));
-            });
+            msg("Data", resp.melody.join(" "));
+        });
         return false;
     });
 
@@ -69,7 +66,7 @@ jQuery(document).ready(function ($) {
         $("#vote").hide();
         $.ajax({
             type: "post",
-            url: "/melody/vote/" + $('#vote').attr("data-melody"),
+            url: "/melody/vote/" + $("#vote").attr("data-melody"),
             data: {vote: "Y"}
         });
     });
@@ -78,7 +75,7 @@ jQuery(document).ready(function ($) {
         $("#vote").hide();
         $.ajax({
             type: "post",
-            url: "/melody/vote/" + $('#vote').attr("data-melody"),
+            url: "/melody/vote/" + $("#vote").attr("data-melody"),
             data: {vote: "N"}
         });
     });
