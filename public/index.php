73f19a8e33ab78aafc00c3b749141b08c23fd45b
Timothy Boronczyk <timothy.boronczyk@optanix.com>
2017-03-11 21:43:40 -0500

    flattening out files

--- public/index.php
+++ public/index.php
@@ -9,20 +9,47 @@ if (PHP_SAPI == 'cli-server') {
 
 chdir(__DIR__);
 require_once '../vendor/autoload.php';
+
 use Slim\App;
 use Slim\Container;
 use Slim\Views\Twig;
+use Zaemis\MusicComposer\Composer;
+use Zaemis\MusicComposer\MidiWriter;
 
-$settings = require '../settings.php';
-error_reporting($settings['php.error_reporting']);
-ini_set('display_errors', $settings['php.display_errors']);
-date_default_timezone_set($settings['php.default_timezone']);
+$settings = [
+    // framework settings
+    'displayErrorDetails' => true,
 
-$container = new Container(['settings' => $settings]);
-require_once '../container.php';
+    // application paths
+    'path.data' => '../data/data.json',
+    'path.templates' => '../templates',
+    'path.training' => '../training'
+];
 
-$app = new App($container);
-$app->view = new Twig($container['settings']['path.templates']);
+$c = new Container(['settings' => $settings]);
+
+$c['composer'] = function ($c) {
+    $composer = new Composer;
+    if (is_readable($c['settings']['path.data'])) {
+        $pitches = file_get_contents($c['settings']['path.data']);
+        $composer->fromJson($pitches);
+    } else {
+        foreach (glob($c['settings']['path.training'] . '/*.txt') as $f) {
+            $pitches = file_get_contents($f);
+            $pitches = array_filter(preg_split('|[\s]+|', $pitches));
+            $composer->tally($pitches);
+        }
+        file_put_contents($c['settings']['path.data'], $composer->toJson());
+    }
+    return $composer;
+};
+
+$c['midiwriter'] = function ($c) {
+    return new MidiWriter;
+};
+
+$app = new App($c);
+$app->view = new Twig($c['settings']['path.templates']);
 
 $app->get('/', function ($req, $resp, $args) use ($app) {
     return $app->view->render($resp, 'index.html', []);
@@ -33,8 +60,7 @@ $app->post('/', function ($req, $resp, $args) use ($app) {
     $start = $data['start'];
     $count = $data['count'];
 
-    $c = $app->getContainer();
-    $composer = $c['composer'];
+    $composer = $this->composer;
     $melody = $composer->compose($start, $count);
 
     return $resp->withHeader('Content-Type', 'application/json')
@@ -44,8 +70,7 @@ $app->post('/', function ($req, $resp, $args) use ($app) {
 $app->get('/midi/{data}', function ($req, $resp, $args) use ($app) {
     $data = explode(',', $args['data']);
 
-    $c = $app->getContainer();
-    $writer = $c['midiwriter'];
+    $writer = $this->midiwriter;
     $midi = $writer->write($data);
 
     return $resp->withHeader('Content-Type', 'application/x-mid')
@@ -59,11 +84,11 @@ $app->post('/vote/{data}', function ($req, $resp, $args) use ($app) {
     $data = explode(',', $args['data']);
 
     $c = $app->getContainer();
-    $composer = $c['composer'];
+    $composer = $this->composer;
 
     if ($vote == 'Y') {
         $composer->tally($data);
-        file_put_contents($c['settings']['path.data'], $composer->toJson());
+        file_put_contents($this->settings['path.data'], $composer->toJson());
     }
 });
 
